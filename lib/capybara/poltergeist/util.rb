1bc71179d0a8bd8e567fb0fef2b9974168fac3e9
Tom Miller <tom.miller@luckie.com>
2012-08-14 12:18:22 -0500

    Hardcode port

--- lib/capybara/poltergeist/util.rb
+++ lib/capybara/poltergeist/util.rb
@@ -3,10 +3,11 @@ require 'socket'
 module Capybara::Poltergeist
   module Util
     def self.find_available_port
-      server = TCPServer.new('127.0.0.1', 0)
-      server.addr[1]
-    ensure
-      server.close if server
+      8080
+    #  server = TCPServer.new('127.0.0.1', 0)
+    #  server.addr[1]
+    #ensure
+    #  server.close if server
     end
   end
 end
